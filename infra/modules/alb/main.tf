c79c7a790bf419a8093c1812dfaae6dbf33386db
Chandler Hagan <gchagan36@gmail.com>
2026-04-21 00:45:40 +0000

    fix(07): WR-01 restrict ALB ingress to VPC CIDR instead of 0.0.0.0/0

--- infra/modules/alb/main.tf
+++ infra/modules/alb/main.tf
@@ -27,7 +27,7 @@ resource "aws_vpc_security_group_ingress_rule" "alb_https" {
   from_port         = 443
   to_port           = 443
   ip_protocol       = "tcp"
-  cidr_ipv4         = "0.0.0.0/0"
+  cidr_ipv4         = var.vpc_cidr_block  # Restrict to VPC CIDR for defence-in-depth (D-05)
 }
 
 resource "aws_vpc_security_group_egress_rule" "alb_to_vpc" {
